aeb5cbf5eca4d6a0550eb4441b131da8f3dfade5
James Turnbulls <james@lovedthanlost.net>
2025-08-30 11:36:48 -0400

    Updated v2.1.1 to fix open window

--- Sources/CentralTimeApp.swift
+++ Sources/CentralTimeApp.swift
@@ -5,8 +5,12 @@ struct CentralTimeApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
-        Settings {
+        WindowGroup {
             EmptyView()
+                .frame(width: 0, height: 0)
+                .hidden()
         }
+        .windowStyle(.hiddenTitleBar)
+        .windowResizability(.contentSize)
     }
 }
